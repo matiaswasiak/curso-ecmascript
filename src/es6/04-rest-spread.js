a718f38228408a57a0726523f53d42cd6ab92950
matiaswasiak <matiaswasiak@gmail.com>
2023-07-17 13:20:24 -0300

    ES6: spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -17,3 +17,31 @@ let person = {
 
 let { name, age } = person;
 console.log(name); // John
+
+// Spread operator
+
+let numbers = [1, 2, 3];
+let newNumbers = [...numbers, 4, 5, 6];
+console.log(newNumbers); // [1, 2, 3, 4, 5, 6]
+
+let otherPerson = {
+  name: "John",
+  age: 25,
+};
+
+let newPerson = {
+  id: 1,
+  ...otherPerson,
+  location: "New York",
+};
+console.log(newPerson); // {id: 1, name: "John", age: 25, location: "New York"}
+
+// Rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3, 4, 5);
