a0840234b41b50d9141bb80689c7caad1f68da2b
matiaswasiak <matiaswasiak@gmail.com>
2023-07-17 13:03:32 -0300

    ES6: strings

---
+++ src/es6/03-strings.js
@@ -0,0 +1,15 @@
+let hello = "Hello";
+let world = "World";
+let boringWay = hello + " " + world;
+
+// Template strings - Template literals
+let fantasticWay = `${hello} ${world}!`;
+console.log(fantasticWay);
+
+// Multiple lines
+let oldWayMultipleLines = "Hello\n" + "World!";
+console.log(oldWayMultipleLines);
+
+let multipleLines = `${hello}
+${world}!`;
+console.log(multipleLines);
