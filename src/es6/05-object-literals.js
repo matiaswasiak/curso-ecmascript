91b6b57a7319cfec1cd8b8eb691612b4bdb257b4
matiaswasiak <matiaswasiak@gmail.com>
2023-07-17 13:30:28 -0300

    ES6: object literals

--- src/es6/05-object-literals.js
+++ src/es6/05-object-literals.js
@@ -1,4 +1,6 @@
 // Enhaced Object Literals
+// What is an object literal?
+// An object literal is a list of zero or more pairs of property names and associated values of an object, enclosed in curly braces ({}).
 
 function newUser(name, age, country) {
   return {
