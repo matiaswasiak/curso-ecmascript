4182f2b0a957f8be3d9b98495760c77e9db79771
matiaswasiak <matiaswasiak@gmail.com>
2023-07-24 08:08:58 -0300

    ES8: object entries y object values

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,8 @@
+const countries = {
+  BR: "Brazil",
+  DE: "Germany",
+  RO: "Romania",
+  US: "United States of America",
+};
+
+console.log(Object.entries(countries));
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,9 @@
+const countries = {
+  BR: "Brazil",
+  DE: "Germany",
+  RO: "Romania",
+  US: "United States of America",
+};
+
+const keys = Object.values(countries);
+console.log(keys); // ["Brazil", "Germany", "Romania", "United States of America"]
