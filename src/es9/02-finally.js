3602032d376ec7ac72208c0ad90850828bfd886f
matiaswasiak <matiaswasiak@gmail.com>
2023-07-24 08:39:35 -0300

    ES9: Promise.finally

---
+++ src/es9/02-finally.js
@@ -0,0 +1,14 @@
+const anotherFunction = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("Hey!");
+    } else {
+      reject("Whoooops!");
+    }
+  });
+};
+
+anotherFunction()
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Finally"));
