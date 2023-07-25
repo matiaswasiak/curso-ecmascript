0802c739b52b5af6a543f6802237ea8f4d49519d
matiaswasiak <matiaswasiak@gmail.com>
2023-07-25 11:37:21 -0300

    ES12: numeric-separators y replaceAll

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,6 @@
+// What is numeric separators in JavaScript?
+
+// Numeric separators are an easy way to make your code more readable by creating a visual separation between groups of digits. Numeric separators are simply underscores (_) that can be placed between digits to make your code more readable.
+
+const value = 100_000_000_000;
+console.log(value);
---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,5 @@
+const string = "JavaScript is a fantastic language. JavaScript is the best!";
+
+const replacedString = string.replaceAll("JavaScript", "JS");
+
+console.log(replacedString);
